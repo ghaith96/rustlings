cde8c63ffe0897c822987966f2539a1483656692
ghaith96 <ghaithjardane@gmail.com>
2020-05-23 11:47:07 +0300

    solve traits exercises

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -1,14 +1,13 @@
 // traits1.rs
 // Time to implement some traits!
-// 
+//
 // Your task is to implement the trait
 // `AppendBar' for the type `String'.
-// 
+//
 // The trait AppendBar has only one function,
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
@@ -16,6 +15,10 @@ trait AppendBar {
 impl AppendBar for String {
     //Add your code here
 
+    fn append_bar(mut self) -> Self {
+        self.push_str("Bar");
+        self
+    }
 }
 
 fn main() {
@@ -40,5 +43,4 @@ mod tests {
             String::from("BarBar")
         );
     }
-
 }
